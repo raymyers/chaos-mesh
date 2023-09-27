841dd3d22d67cc9647c06173fb18d344d23eaae4
Mohammad <jamshidi.m799@gmail.com>
2023-09-27 10:44:36 +0330

    feat: remove token when get Unauthorized (#4193)
    * feat: remove token when get Unauthorized
    
    Signed-off-by: Mohammad Jamshidi <jamshidi.m799@gmail.com>
    
    * update CHANGELOG.md
    
    Signed-off-by: Mohammad Jamshidi <jamshidi.m799@gmail.com>
    
    * fix: clear all connected caches
    
    Signed-off-by: Yue Yang <g1enyy0ung@gmail.com>
    
    * fix: resetAPIAuthentication
    
    Signed-off-by: Yue Yang <g1enyy0ung@gmail.com>
    
    ---------
    
    Signed-off-by: Mohammad Jamshidi <jamshidi.m799@gmail.com>
    Signed-off-by: Yue Yang <g1enyy0ung@gmail.com>
    Co-authored-by: Yue Yang <g1enyy0ung@gmail.com>

--- ui/app/src/components/TopContainer/Auth.tsx
+++ ui/app/src/components/TopContainer/Auth.tsx
@@ -19,7 +19,7 @@ import KeyIcon from '@mui/icons-material/Key'
 import { Box, Button, Divider, IconButton, Link, Typography } from '@mui/material'
 import { Stale } from 'api/queryUtils'
 import { useGetCommonConfig } from 'openapi'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 import ConfirmDialog from '@ui/mui-extends/esm/ConfirmDialog'
@@ -33,10 +33,9 @@ import { OidcAuthService } from '../../oidc/OidcAuthService'
 
 interface AuthProps {
   open: boolean
-  setOpen: React.Dispatch<React.SetStateAction<boolean>>
 }
 
-const Auth: React.FC<AuthProps> = ({ open, setOpen }) => {
+const Auth: React.FC<AuthProps> = ({ open }) => {
   const navigate = useNavigate()
 
   const { data: config } = useGetCommonConfig({
@@ -47,10 +46,6 @@ const Auth: React.FC<AuthProps> = ({ open, setOpen }) => {
   })
 
   const [tokenGenOpen, setTokenGenOpen] = useState(false)
-
-  useEffect(() => {
-    setOpen(open)
-  }, [open, setOpen])
 
   const handleSubmitCallback = () => navigate(0)
   const handleAuthGCP = () => (window.location.href = '/api/auth/gcp/redirect')
