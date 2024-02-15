cd850877b3fcfdcd60d9890c37cc0efcf165156c
Kevin Wallace <Kevin Wallace>
2024-02-15 10:19:20 -0800

    Add OIDC_CLIENT_ID to deployment yaml and install script

--- pkg/dashboard/apiserver/auth/oidc/service.go
+++ pkg/dashboard/apiserver/auth/oidc/service.go
@@ -16,6 +16,12 @@
 package oidc
 
 import (
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/base64"
+	"encoding/hex"
+	"fmt"
+	"io"
 	"net/http"
 	"net/url"
 	"path"
@@ -30,10 +36,11 @@ import (
 )
 
 type Service struct {
-	clientId  string
-	authority string
-	rootUrl   *url.URL
-	logger    logr.Logger
+	clientId     string
+	authority    string
+	codeVerifier string
+	rootUrl      *url.URL
+	logger       logr.Logger
 }
 
 // OidcConfig is separate from the Oauth configurations currently available
@@ -114,24 +121,97 @@ func (s *Service) getOidcConfig(c *gin.Context) oauth2.Config {
 	}
 }
 
+// PKCE requires several random values to be generated and passed, so we will enable those here
+// code adapted from: https://chrisguitarguy.com/2022/12/07/oauth-pkce-with-go/
+func randomBytesInHex(count int) (string, error) {
+	buf := make([]byte, count)
+	_, err := io.ReadFull(rand.Reader, buf)
+	if err != nil {
+		return "", fmt.Errorf("could not generate %d random bytes: %v", count, err)
+	}
+
+	return hex.EncodeToString(buf), nil
+}
+
+type AuthURL struct {
+	url          string
+	state        string
+	codeVerifier string
+}
+
+func (u *AuthURL) String() string {
+	return u.url
+}
+
+func authorizationURL(config *oauth2.Config) (*AuthURL, error) {
+	codeVerifier, verifierErr := randomBytesInHex(32) // 64 char string
+	if verifierErr != nil {
+		return nil, fmt.Errorf("could not create a code verifier: %v", verifierErr)
+	}
+	sha2 := sha256.New()
+	io.WriteString(sha2, codeVerifier)
+	codeChallenge := base64.RawURLEncoding.EncodeToString(sha2.Sum(nil))
+
+	state, stateErr := randomBytesInHex(24)
+	if stateErr != nil {
+		return nil, fmt.Errorf("could not generate random state: %v", stateErr)
+	}
+
+	authUrl := config.AuthCodeURL(
+		state,
+		oauth2.SetAuthURLParam("code_challenge_method", "S256"),
+		oauth2.SetAuthURLParam("code_challenge", codeChallenge),
+	)
+
+	return &AuthURL{
+		url:          authUrl,
+		state:        state,
+		codeVerifier: codeVerifier,
+	}, nil
+}
+
 func (s *Service) handleRedirect(c *gin.Context) {
 	oauth := s.getOidcConfig(c)
-	uri := oauth.AuthCodeURL("", oauth2.AccessTypeOffline, oauth2.ApprovalForce)
+	// for PKCE we need to amend the URI as stored in the configuration
+	uri, uriErr := authorizationURL(&oauth)
+	if uriErr != nil {
+		// TODO: Do not ship this; the error returned in a stack trace.
+		c.AbortWithError(http.StatusInternalServerError, uriErr)
+	}
 
-	c.Redirect(http.StatusFound, uri)
+	// TODO: Need to store the URL, state, and *codeVerifier*
+	// Do I need the URL?
+	//c.SetCookie("oidc_state", uri.state, 0, "", "", false, false)
+	//c.SetCookie("oidc_verifier", uri.codeVerifier, 0, "", "", false, false)
+	// TODO: Answer this question:
+	// Does storing it within the Service mean it's not restricted to a single user?
+	// And thus opens us up to race conditions?
+	// Should I pursue storing it within the header as a cookie and retrieving it instead?
+	s.codeVerifier = uri.codeVerifier
+
+	c.Redirect(http.StatusFound, uri.String())
 }
 
 func (s *Service) authCallback(c *gin.Context) {
 	ctx := c.Request.Context()
 
 	oidc := s.getOidcConfig(c)
-	oauth2Token, err := oidc.Exchange(ctx, c.Request.URL.Query().Get("code"), oauth2.AccessTypeOffline, oauth2.ApprovalForce)
+	oauth2Token, err := oidc.Exchange(
+		ctx,
+		c.Request.URL.Query().Get("code"),
+		oauth2.SetAuthURLParam("code_verifier", s.codeVerifier),
+	)
 	if err != nil {
+		// TODO: Here's where we're not getting the codeVerifier currently;
 		utils.SetAPIError(c, utils.ErrInternalServer.WrapWithNoMessage(err))
 		return
 	}
 
 	setCookie(c, oauth2Token)
+	// TODO: The token is not a valid one, and is empty afterwards ha ha ha ha.
+	// "Unauthorized: Check validity of token"
+	// This leads me to suspect that while we are retrieving a valid Okta/Oauth2 token,
+	// it does not include within it the data we need for kubernetes STUFF
 	target := url.URL{
 		Path: "/",
 	}
